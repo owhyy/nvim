98d7abe011f24d2b542e1c21c872cd571e3f4e38
Babin Ion <ionbabin123@mail.ru>
2022-03-23 09:05:03 +0200

    new theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -79,6 +79,8 @@ return require('packer').startup(function(use)
   -- Latex
   use "lervag/vimtex"
 
+  -- black colortheme
+  use "aditya-azad/candle-grey"
 
   use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
--- lua/themes.lua
+++ lua/themes.lua
@@ -1,2 +1,4 @@
-vim.g.solarized_italic_comments = true
-require('solarized').set('dark')
+-- vim.g.solarized_italic_comments = true
+-- require('solarized').set('dark')
+
+vim.cmd("colorscheme candle-grey")
