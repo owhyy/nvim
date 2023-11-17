ef9be934258a4164f4a56293a1972ac43213dac8
owhyy <ionbabin123@mail.ru>
2023-11-17 10:22:41 +0200

    feat: add keybindings

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,4 +6,11 @@ cmp.setup({
 	}, {
 		{ name = "buffer" },
 	}),
+	mapping = cmp.mapping.preset.insert({
+		["<C-p>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+    ["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
+    ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+    ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+	})
 })
