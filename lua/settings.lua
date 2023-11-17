fa8b556a5500ef5545b74f9492de94d10863758e
owhyy <ionbabin123@mail.ru>
2023-11-17 15:40:33 +0200

    feat: highlightyank

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,9 +1,9 @@
 vim.g.mapleader = " "
 vim.o.clipboard = "unnamedplus"
 vim.o.hlsearch = false
-vim.o.shiftwidth=2
-vim.o.tabstop=2
-vim.o.softtabstop=2
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
 
 vim.wo.number = true
 vim.wo.relativenumber = true
@@ -11,3 +11,5 @@ vim.wo.relativenumber = true
 -- recommended by nvim-tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+vim.cmd([[au TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout=300})]])
