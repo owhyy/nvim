f06410a945038731e9d7ba806dff0078e23ca99b
owhyy <ionbabin123@mail.ru>
2023-12-26 11:11:38 +0200

    feat: add new keybindings for gitsigns

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,12 +1,18 @@
 local builtin = require("telescope.builtin")
 local zk = require("zk.commands")
 local nt = require("nvim-tree.api")
+local gs = require("gitsigns")
 
 -- Telescope
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fz", zk.get("ZkNotes"), {})
+vim.keymap.set("n", "<leader>fl", builtin.resume, {})
+
+vim.keymap.set("n", "<leader>gs", gs.stage_hunk)
+vim.keymap.set("n", "<leader>gr", gs.reset_hunk)
+vim.keymap.set("n", "<leader>gb", gs.reset_hunk)
 
 -- Lsp
 vim.api.nvim_create_autocmd("LspAttach", {
